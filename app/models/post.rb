e12483615e6d879da33f29d8b1d0ba9f2a6fc559
SaintNerevar <shivahegde@rocketmail.com>
2020-03-19 18:37:14 +0530

    delete deprecated line

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,4 @@ class Post < ApplicationRecord
     validates :image, presence: true
 
     has_one_attached :image
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
