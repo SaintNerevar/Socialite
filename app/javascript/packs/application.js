f6045503242b8274f52fd7c919410158f6116dc1
SaintNerevar <shivahegde@rocketmail.com>
2020-03-23 01:22:48 +0530

    Iron out Javascript Bugs
    Problem is a rogue import line in the Application.js file.
    Delete that line and some uneeded, redundant lines in other files.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import "bootstrap"
-import "../stylesheets/application"
 
 document.addEventListener("turbolinks:load", () => {
     $('[data-toggle="tooltip"]').tooltip();
